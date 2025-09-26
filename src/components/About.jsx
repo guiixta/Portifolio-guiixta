b6f21a4a91c38bb1c417ee62f0b4433b093e920a
guiixta <ferreiragui@proton.me>
2025-09-26 19:22:54 -0300

    New Sobre mim

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
     <section id="about" className="container mx-auto p-8 my-8 bg-white rounded-lg shadow-md">
       <h2 className="text-4xl font-bold text-gray-800 mb-6 text-center">Sobre Mim</h2>
       <p className="text-lg text-gray-700 leading-relaxed text-justify max-w-3xl mx-auto">
-        Olá e bem-vindo(a) ao meu portfólio! Sou um desenvolvedor movido pela curiosidade e pela paixão de resolver problemas. Embora minha experiência inicial seja com o ecossistema web, utilizando React, JavaScript e PHP para dar vida a projetos, minha verdadeira motivação é o processo de aprender e construir soluções codando.
+        Olá e bem-vindo(a) ao meu portfólio! Sou um desenvolvedor movido pela curiosidade e pela paixão de resolver problemas. Minha experiência começou com o ecossistema web, utilizando React, JavaScript e PHP para dar vida a projetos, e se expandiu para a criação de dashboards interativos com Python. Minha verdadeira motivação é o processo de aprender e construir soluções, independentemente da tecnologia.
 
-Estou em busca de uma vaga de estágio onde eu possa não apenas aplicar o que já sei, mas, principalmente, ser desafiado a mergulhar em novas tecnologias. Tenho grande capacidade de aprendizado e estou pronto para me especializar na stack da equipe e agregar valor em qualquer frente do desenvolvimento de software.
+  Estou em busca de uma vaga de estágio onde eu possa não apenas aplicar o que já sei, mas, principalmente, ser desafiado a mergulhar em novas ferramentas. Tenho grande capacidade de aprendizado e estou pronto para me especializar na stack da equipe e agregar valor em qualquer frente do desenvolvimento de software.
 
 Vamos construir algo incrível juntos? Explore meus projetos e vamos conversar!</p>
     </section>
