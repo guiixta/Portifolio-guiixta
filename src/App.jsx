b2816afed18902f7a1647e86d31a42e755045e4d
guiixta <ferreiragui@proton.me>
2025-09-26 12:36:52 -0300

    Adicionar projetos em python e mais algums detalhes de estilização

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,35 @@ function App() {
       <Header />
       <main>
         <About />
-        <Projects> 
-          <ItemProject titulo="Mercanota" descricao="Plataforma web local para criação de relatórios de envio de mercadoria." link="/sobre?projeto=Mercanota">
-            <Link className="bg-gray-800 flex justify-center items-center text-center px-[5px] py-[3px] rounded-sm" to="https://github.com/guiixta/Mercanota"><i className="text-white bi bi-github"></i></Link>  
-          </ItemProject>
-          <ItemProject titulo="Banco Academico" descricao="Plataforma para entrega centralizada de atividades escolares." link="/sobre?projeto=BancoAcademico"> 
-            <Link className="bg-gray-800 flex justify-center items-center text-center px-[5px] py-[3px] rounded-sm" to="http://bancoacademico.educacao.ws/"><i className="text-white bi bi-arrow-up-right-square-fill"></i></Link>
-          </ItemProject>
+        <Projects>
+          <div className='bg-blue-500 rounded-lg shadow p-3 w-full'>
+            <div className='w-full flex flex-col mb-3'>
+              <h1 className='m-0 font-bold text-white text-2xl'>ReactJS</h1>
+              <span className='p-[0.5px] bg-white w-full flex'></span>
+            </div>
+            <div className='flex flex-col gap-2'>
+              <ItemProject titulo="Mercanota" descricao="Plataforma web local para criação de relatórios de envio de mercadoria." link="/sobre?projeto=Mercanota">
+                <Link className="bg-gray-800 flex justify-center items-center text-center px-[5px] py-[3px] rounded-sm hover:bg-gray-900" to="https://github.com/guiixta/Mercanota"><i className="text-white bi bi-github"></i></Link>  
+              </ItemProject>
+              <ItemProject titulo="Banco Academico" descricao="Plataforma para entrega centralizada de atividades escolares." link="/sobre?projeto=BancoAcademico"> 
+                <Link className="bg-gray-800 flex justify-center items-center text-center px-[5px] py-[3px] rounded-sm hover:bg-gray-900" to="http://bancoacademico.educacao.ws/"><i className="text-white bi bi-arrow-up-right-square-fill"></i></Link>
+              </ItemProject>
+            </div>
+          </div>
+
+          <div className='bg-amber-500 rounded-lg shadow p-3 w-full'>
+            <div className='w-full flex flex-col mb-3'>
+              <h1 className='m-0 font-bold text-2xl'>Python</h1>
+              <span className='p-[0.5px] bg-black w-full flex'></span>
+            </div>
+            <div className='flex flex-col gap-2'>
+              <ItemProject titulo="Dashboard GEE" descricao="Dashboard interativo para visualização de emissões de gases de efeito estufa." link="/sobre?projeto=DashSEEG">
+                <Link className="bg-gray-800 flex justify-center items-center text-center px-[5px] py-[3px] rounded-sm hover:bg-gray-900" to="https://github.com/guiixta/dash-emissao-gas-seeg"><i className="text-white bi bi-github"></i></Link> 
+                <Link className="bg-gray-800 flex justify-center items-center text-center px-[5px] py-[3px] rounded-sm hover:bg-gray-900" to="https://dash-efeitoestufa-seeg.vercel.app/"><i className="text-white bi bi-arrow-up-right-square-fill"></i></Link>
+              </ItemProject>
+              
+            </div>
+          </div>
         </Projects>
         <Contact />
       </main>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = (props) => {
   return (
     <section id="projects" className="container mx-auto p-8 my-8 bg-gray-100 rounded-lg shadow-md">
       <h2 className="text-4xl font-bold text-gray-800 mb-8 text-center">Meus Projetos</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="flex flex-col gap-2 w-full flex-grow overflow-y-auto max-h-120">
         {props.children} 
       </div>
     </section>
@@ -14,10 +14,12 @@ const Projects = (props) => {
 export function ItemProject(props){
   return(
     <>
-      <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-        <div className="flex justify-between">
+      <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl/30 transition-shadow duration-300">
+        <div className="flex justify-between items-center">
           <h3 className="text-2xl font-semibold text-gray-900">{props.titulo}</h3>
-          {props.children}
+          <div className='flex items-center gap-1'> 
+            {props.children}
+          </div>
         </div>
         <p className="text-gray-700 mb-4">{props.descricao}</p>
         <a href={props.link} className="text-blue-600 hover:text-blue-800 font-medium" target="_blank" rel="noopener noreferrer">
